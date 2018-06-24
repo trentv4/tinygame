a3ab45bb188cebadbb8440a6723237bd2e1f2435
trentv4 <trentvanslyke@gmail.com>
2018-06-24 07:09:10 -0400

    Remove unnecessary headers, fix compile script failing in the morning, sort out linking and libs needed

--- compile.bat
+++ compile.bat
@@ -1,10 +1,11 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+set mytime=%mytime: =0%
 set filename=build_%mydate%_%mytime%
 echo.
 echo -------------- GCC --------------
-gcc -Wall -o build/%filename% src/*.c -L ".\libs" -lglfw3
+gcc -Wall -o build/%filename% src/*.c -L ".\libs" -lglfw3 -lopengl32 -lgdi32
 echo.
 echo ----- %filename% -----
 .\build\%filename%
