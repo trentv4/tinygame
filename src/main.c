3d1f18fa09fd58640f11bc01575e46a823948b5f
trentv4 <trentvanslyke@gmail.com>
2018-06-24 07:19:37 -0400

    Create initial window creation and gl context

--- src/main.c
+++ src/main.c
@@ -4,10 +4,24 @@
 int main()  
 {  
 	printf("Program started\n");
-	if(!glfwInit())
-	{
-		printf("GLFW was not initialized.\n");
-	}
+	if(!glfwInit()) printf("GLFW was not initialized.\n");
+
 	printf("GLFW initialized.\n");
+
+	// if needed, glfwWindowHint glfw_context_version_maj/min 2 for min version
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Window", NULL, NULL);
+	if(!window) printf("Unable to create window");
+	printf("Window created.");
+
+	glfwMakeContextCurrent(window);
+	//glad goes here to load extensions if needed
+
+	while(!glfwWindowShouldClose(window))
+	{
+
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
     return 0;
-}   
+}  
